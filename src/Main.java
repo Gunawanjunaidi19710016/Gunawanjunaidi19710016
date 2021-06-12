4df8fac8a02344fa86c22a0a7371a2fe167f7469
Gunawan <gunawanjndreal06@gmail.com>
2021-06-12 14:06:15 +0700

    PROJECT IDEA

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+        public static void main (String[] args) {
+            String namaDepan = "Gunawan";
+            String namaTengah = "Junaidi";
+            String namaBelakang = "Real";
+
+            System.out.print(namaDepan + namaTengah + namaBelakang);
+        }
+}
