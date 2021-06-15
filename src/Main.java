a1c4290d78257bc768178e4091556f89d7999bdd
Gunawan <gunawanjndreal06@gmail.com>
2021-06-15 10:25:50 +0700

    Membuat String dari Array Char

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main{
             String namaDepan = "Gunawan";
             String namaTengah = "Junaidi";
             String namaBelakang = "Real";
+            int usia = 19;
+            int targetTahunKuliah = 4;
+            double ipk = 3.987654321;
+            char nilaiAbjad = 'A';
+            boolean tampan = true;
 
             System.out.print(namaDepan + namaTengah + namaBelakang);
 
